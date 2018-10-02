df05a29ec72b444fa453ae4332672fa385483853
tankztz <tankztz@gmail.com>
2018-10-02 14:51:24 +0800

    Change base url

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'localhost/CodeIgniter-3.1.9';
+$config['base_url'] = 'http://localhost/CS2102-Stuff-Sharing/';
 
 /*
 |--------------------------------------------------------------------------

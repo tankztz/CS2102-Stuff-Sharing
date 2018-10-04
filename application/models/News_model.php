b22ae2a70a50a7e2a718a8daad6bc7b34f0b5376
tankztz <tankztz@gmail.com>
2018-10-04 16:15:55 +0800

    Code style minor fix

--- application/models/News_model.php
+++ application/models/News_model.php
@@ -1,34 +1,34 @@
 <?php
 class News_model extends CI_Model {
 
-        public function __construct()
+    public function __construct()
+    {
+        $this->load->database();
+    }
+
+    public function get_news($slug = FALSE)
+    {
+        if ($slug === FALSE)
         {
-                $this->load->database();
+            $query = $this->db->get('news');
+            return $query->result_array();
         }
 
-        public function get_news($slug = FALSE)
-        {
-            if ($slug === FALSE)
-            {
-                    $query = $this->db->get('news');
-                    return $query->result_array();
-            }
+        $query = $this->db->get_where('news', array('slug' => $slug));
+        return $query->row_array();
+    }
 
-            $query = $this->db->get_where('news', array('slug' => $slug));
-            return $query->row_array();
-        }
+    public function set_news()
+    {
+        $this->load->helper('url');
 
-        public function set_news()
-        {
-            $this->load->helper('url');
+        $data = array(
+        'id' => $this->input->post('id'),
+        'title' => $this->input->post('title'),
+        'slug' => $this->input->post('slug'),
+        'text' => $this->input->post('text')
+        );
 
-            $data = array(
-                'id' => $this->input->post('id'),
-                'title' => $this->input->post('title'),
-                'slug' => $this->input->post('slug'),
-                'text' => $this->input->post('text')
-            );
-
-            return $this->db->insert('news', $data);
-        }
+        return $this->db->insert('news', $data);
+    }
 }

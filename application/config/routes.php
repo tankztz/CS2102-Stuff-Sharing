4f833d28128ca62e3183b15d843de715fa46e5ad
Jia Yilin <e0007663@u.nus.edu>
2018-10-06 17:09:56 +0800

    add users/login into routes

--- application/config/routes.php
+++ application/config/routes.php
@@ -55,6 +55,8 @@ $route['news/create'] = 'news/create';
 $route['news/(:any)'] = 'news/view/$1';
 $route['news'] = 'news';
 
+$route['users/login'] = 'users/login';
+$route['users/index'] = 'users/index';
 $route['users/create'] = 'users/create';
 $route['users/(:any)'] = 'users/view/$1';
 $route['users'] = 'users';

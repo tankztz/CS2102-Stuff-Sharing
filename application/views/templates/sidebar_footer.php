5b1be3c136dbeb0612cb14f98f90e1b8f5cd037f
Jia Yilin <e0007663@u.nus.edu>
2018-10-06 20:59:50 +0800

    Add quick links

--- application/views/templates/sidebar_footer.php
+++ application/views/templates/sidebar_footer.php
@@ -5,11 +5,13 @@
 					<div class="widget">
 						<h3>Quick Links</h3>
 						<ul>
-						<li><a href="http://localhost/CS2102-Stuff-Sharing/index.php/users">Users</a></li>
+						<li><a href="http://localhost/CS2102-Stuff-Sharing/index.php/users/index">Users</a></li>
 						<li><a href="http://localhost/CS2102-Stuff-Sharing/index.php/news">News</a></li>
-						<li><a href="#">Link 3</a></li>
-						<li><a href="#">Link 4</a></li>
-						<li><a href="#">Link 5</a></li>
+						<li><a href="http://localhost/CS2102-Stuff-Sharing/index.php/item/index">Item</a></li>
+						<li><a href="http://localhost/CS2102-Stuff-Sharing/index.php/bid/index">Bid</a></li>
+						<li><a href="http://localhost/CS2102-Stuff-Sharing/index.php/post/index">Post</a></li>
+						<li><a href="http://localhost/CS2102-Stuff-Sharing/index.php/loan/index">Loan</a></li>
+						<li><a href="http://localhost/CS2102-Stuff-Sharing/index.php/comment/index">Comment</a></li>
 						</ul>
 					</div>
 					</nav>

ec13e513a27ffc7c767f32f0442e334f9775b0e4
Jia Yilin <e0007663@u.nus.edu>
2018-11-05 23:37:22 +0800

    Remove minimum point from post

--- application/views/bid/index.php
+++ application/views/bid/index.php
@@ -24,7 +24,8 @@
             <div class="card m-3">
                 <div class="card-body">
                     <h4 class="card-title">Bid Points Placed:<?php echo $users_bid['points']; ?></h4>
-                    <a href="<?php echo site_url('post/'.$users_post['post_id']); ?>" class="btn btn-primary m-3">Choose</a>
+                    <p class="card-text">Bidder:<?php echo $users_bid['username']; ?></p>
+                    <a href="<?php echo site_url('bid/confirm_bidder/'.$users_bid['post'].'/'.$users_bid['bidder']); ?>" class="btn btn-primary m-3">Choose</a>
 
                 </div>
             </div>
--- application/views/post/myindex.php
+++ application/views/post/myindex.php
@@ -8,7 +8,6 @@
                 <div class="card-body">
                     <h4 class="card-title"><?php echo $users_post['title']; ?></h4>
                     <p class="card-text"><?php echo $users_post['description']; ?></p>
-                    <p class="card-text"><small class="text-muted"><?php echo $users_post['minimum_bid']; ?></small></p>
                     <div class="row">
                     <a href="<?php echo site_url('post/'.$users_post['post_id']); ?>" class="btn btn-primary m-3">View Details</a>
                     <a href="<?php echo site_url('bid/view_bidunderpost/'.$users_post['post_id']); ?>" class="btn btn-primary m-3">Choose Bidders</a>

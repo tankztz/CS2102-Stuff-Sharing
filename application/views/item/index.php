f6399819bcc1e3e873a94242bbe3ce075127fe93
tankztz <tankztz@gmail.com>
2018-11-06 16:15:12 +0800

    Make view item page dynamic show update/view button

--- application/views/item/index.php
+++ application/views/item/index.php
@@ -1,14 +1,27 @@
-<h2><?php echo $title; ?></h2>
 
+<h2 class="text-center"><?php echo $title; ?></h2>
+
+<div class="container">
+<div class="row">
 <?php foreach ($item as $users_item): ?>
-
-        <h3><?php echo $users_item['name']; ?></h3>
-        <div class="main">
-                <?php echo $users_item['owner']; ?>
-                <?php echo $users_item['category']; ?>
-                <?php echo $users_item['description']; ?>
-        </div>
-        <p><a href="<?php echo site_url('item/'.$users_item['item_id']); ?>">View Details</a></p>
-        <p><?php echo site_url('item/'.$users_item['item_id']); ?></p>
+		<div class="col-sm-6">
+            <div class="card">
+                <div class="card-body">
+                    <h4 class="card-title"><?php echo $users_item['name']; ?></h4>
+                    <p class="card-text">category:<?php echo $users_item['category']; ?></p>
+                    <p class="card-text"><?php echo $users_item['description']; ?></p>
+                    <div class="row">
+                    <?php if ($users_post['owner'] == $this->session->userdata('user_id')): ?>
+                        <a href="<?php echo site_url('post/'.$users_item['item_id']); ?>" class="btn btn-primary m-3">Update item(havnt finish)</a>
+                    <?php else: ?>
+                        <a href="<?php echo site_url('post/'.$users_item['item_id']); ?>" class="btn btn-primary m-3">View item(havnt finish)</a>
+                    <?php endif; ?>
+                    </div>
+                    </div>
+                </div>
+            </div>
+      
 
 <?php endforeach; ?>
+</div>
+</div>

d0a21c1f4adda5d9f89fa86849ea8c4b9726de6e
tankztz <tankztz@gmail.com>
2018-11-08 14:37:30 +0800

    Fix back to my post bug

--- application/views/bid/button.php
+++ application/views/bid/button.php
@@ -1,3 +1,3 @@
-<a href="<?php echo base_url(); ?>index.php/users/myposts"> 
+<a href="<?php echo base_url(); ?>index.php/post/index"> 
 	<button type="button" id="myBtn" class="btn btn-success">Return to my posts</button>
 </a>
